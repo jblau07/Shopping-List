12be08ede01f6aea576b95f0ef6556509590c9e8
jblau07 <jlau7248@gmail.com>
2018-01-19 15:32:22 -1000

    completed 2nd test

--- js/tests/shopping_list_test.js
+++ js/tests/shopping_list_test.js
@@ -1,8 +1,14 @@
 // //is a class
+var expect = chai.expect;
+var shold = chai.should();
 
 describe('ShoppingListItem', function () {
-  it('should be a function', function () {
-    expect(ShoppingListItem).to.be.a('function');
-
+  it('should be a class', function () {
+    expect(ShoppingListItem).to.be.a('function')
   });
+  
+    it('should have a property called "name', function (){
+    var tomato = new ShoppingListItem("Tomato");
+    tomato.name.should.equal('Tomato');
+    });
 });
