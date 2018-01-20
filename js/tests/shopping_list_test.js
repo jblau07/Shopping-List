b177b211195a45acc111eaa0cc6c69ecedecc0db
matukai <matt.tanji@gmail.com>
2018-01-19 15:49:24 -1000

    3rd test

--- js/tests/shopping_list_test.js
+++ js/tests/shopping_list_test.js
@@ -3,12 +3,19 @@ var expect = chai.expect;
 var shold = chai.should();
 
 describe('ShoppingListItem', function () {
+  let firstParam = 'apple';
+  let secParam = 'fuji';
+  let firstItem = new ShoppingListItem(firstParam, secParam);
+  
   it('should be a class', function () {
     expect(ShoppingListItem).to.be.a('function')
   });
-  
-    it('should have a property called "name', function (){
-    var tomato = new ShoppingListItem("Tomato");
-    tomato.name.should.equal('Tomato');
-    });
+
+  it('should have a property called name', function () {
+   expect(firstItem.name).to.equal(firstParam)
+  });
+
+  it('should have a property called description', function () {
+    expect(firstItem.description).to.equal(secParam);
+  });
 });
