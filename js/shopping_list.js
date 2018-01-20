977dbd2137392b0ccc66daf8c6dab7b72dba3744
jblau07 <jlau7248@gmail.com>
2018-01-20 12:17:38 -1000

    completed addItem tests and answers

--- js/shopping_list.js
+++ js/shopping_list.js
@@ -1,9 +1,13 @@
 class ShoppingList {
   constructor(items) {
     this.items = [];
-   
+
   }
   addItem(x) {
-    this.items.push(x)
+    if (x instanceof ShoppingListItem) {
+      this.items.push(x)
+    } else {
+      throw new error;
+    }
   }
-}
+  }
--- js/tests/shopping_list_test.js
+++ js/tests/shopping_list_test.js
@@ -3,89 +3,104 @@ var expect = chai.expect;
 var shold = chai.should();
 
 describe('ShoppingListItem', function () {
-  let firstParam = 'apple';
-  let secParam = 'fuji';
-  let firstItem = new ShoppingListItem(firstParam, secParam);
+let firstParam = 'apple';
+let secParam = 'fuji';
+let firstItem = new ShoppingListItem(firstParam, secParam);
 
-  it('should be a class', function () {
-    expect(ShoppingListItem).to.be.a('function')
-  });
-
-  it('should have a property called name', function () {
-    expect(firstItem.name).to.be.a('string');
-  });
-
-  it('should have a property called description', function () {
-    expect(firstItem.description).to.be.a('string');
-  });
-
-  it('should have a property called is_done', function () {
-    expect(firstItem.is_done).to.equal(false);
-  });
-
-  it('should set first argument as the name value', function () {
-    expect(firstItem.name).to.equal(firstParam);
-  });
-
-  it('should set second argument as the description value', function () {
-    expect(firstItem.description).to.equal(secParam);
-  });
-
-  describe('check', function () {
-
-    it('should have a method called check', function () {
-      expect(firstItem.check).to.be.a('function');
-    });
-
-    it('should set is_done to true', function () {
-      firstItem.check();
-      firstItem.is_done.should.equal(true);
-    })
-
-    describe('uncheck', function () {
-      it('should have a method named uncheck', function () {
-        expect(firstItem.uncheck).to.be.a('function')
-      });
-      it('should set is_done to false', function () {
-        firstItem.uncheck();
-        firstItem.is_done.should.equal(false);
-      })
-    })
-    describe('render', function () {
-      it('should have a method name render', function() {
-        expect(firstItem.render).to.be.a('function');
-      })
-      // it('should return a string', function() {
-      //   expect(firstItem.render()).to.be.a('string');
-      // })
-
-      // it('should be wrapped in li tags', function() {
-      //   expect(firstItem.render()).to.equal(<li></li>);
-      // })
-    })
-
-  });
+it('should be a class', function () {
+  expect(ShoppingListItem).to.be.a('function')
 });
+
+it('should have a property called name', function () {
+  expect(firstItem.name).to.be.a('string');
+});
+
+it('should have a property called description', function () {
+  expect(firstItem.description).to.be.a('string');
+});
+
+it('should have a property called is_done', function () {
+  expect(firstItem.is_done).to.equal(false);
+});
+
+it('should set first argument as the name value', function () {
+  expect(firstItem.name).to.equal(firstParam);
+
+});
+
+it('should set second argument as the description value', function () {
+  expect(firstItem.description).to.equal(secParam);
+});
+
+describe('check', function () {
+
+  it('should have a method called check', function () {
+    expect(firstItem.check).to.be.a('function');
+  });
+
+  it('should set is_done to true', function () {
+    firstItem.check();
+    firstItem.is_done.should.equal(true);
+  })
+
+  describe('uncheck', function () {
+    it('should have a method named uncheck', function () {
+      expect(firstItem.uncheck).to.be.a('function')
+    });
+    it('should set is_done to false', function () {
+      firstItem.uncheck();
+      firstItem.is_done.should.equal(false);
+    })
+  })
+  describe('render', function () {
+    it('should have a method name render', function () {
+      expect(firstItem.render).to.be.a('function');
+    })
+    // it('should return a string', function() {
+    //   expect(firstItem.render()).to.be.a('string');
+    // })
+
+    // it('should be wrapped in li tags', function() {
+    //   expect(firstItem.render()).to.equal(<li></li>);
+    // })
+  })
+
+
+})
 let firstList = new ShoppingList();
 
-describe('ShoppingList', function(){
+describe('ShoppingList', function () {
   it('should be a class', function () {
     expect(ShoppingList).to.be.a('function')
   });
   it('should have a property called items', function () {
     expect(firstList).to.have.all.keys('items');
   });
-  it('should initialize items as an empty array', function() {
+  it('should initialize items as an empty array', function () {
     expect(firstList.items).to.be.an('array');
   })
   describe('addItem', function () {
-    let newItem = 'avocado'
+
+
     it('should have a method named addItem', function () {
       expect(firstList.addItem).to.be.a('function')
     });
-    it('should accept one argument', function() {
-      firstList.addItem(newItem);
-      firstList.items.should.include(newItem)
-    })
+    it('should only take one argument', function () {
+      firstList.addItem(firstItem);
+
+    });
+    it('should only accept an argument thats a shopping list item', function () {
+      expect(firstItem instanceof ShoppingListItem).to.equal(true);
+      expect(firstList.addItem.bind(firstList, firstItem)).to.not.throw();
+    });
+    it('should throw an error if a shopping list item is not passed as the argument', function(){
+    expect(firstList.addItem.bind(firstList, 'z')).to.throw();
+
   });
+  it('should add the item to the shopping list', function () {
+    firstList.addItem(firstItem);
+    firstList.items.should.include(firstItem)
+  })
 })
+});
+});
